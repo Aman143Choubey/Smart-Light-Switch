7abc1187441f430d9d274e3c2f493a95da82f0b7
Aman143Choubey <108986366+Aman143Choubey@users.noreply.github.com>
2022-12-01 19:05:55 +0530

    Add files via upload

---
+++ daytime and nighttime = lights on or off.py
@@ -0,0 +1,19 @@
+# Night script
+is_day = False
+lights_on = not is_day
+
+print("Daytime?")
+print(is_day)
+print("Lights on?")
+print(lights_on) 
+
+print("As Time Change")
+
+# Daytime
+is_day = True
+lights_on = not is_day
+
+print("Daytime?")
+print(is_day)
+print("Lights on?")
+print(lights_on)
